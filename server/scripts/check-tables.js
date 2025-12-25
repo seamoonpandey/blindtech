c858d9bd1bd3f6a54db5f18d9eb82efb6c7d1e2e
sussybaka <yousuckatprogrammingbaka@localhost>
2025-12-25 13:12:06 +0545

    chore: remove obsolete server scripts related to team and lottery management

--- server/scripts/check-tables.js
+++
@@ -1,7 +0,0 @@
-const { Client } = require('pg');
-const client = new Client({ connectionString: 'postgres://postgres@localhost:5432/blindtech' });
-client.connect();
-client.query("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'", (err, res) => {
-  console.log(res ? res.rows : err);
-  client.end();
-});
